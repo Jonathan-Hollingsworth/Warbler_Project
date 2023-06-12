c7e239db30fda3169b344cf0e49bb31484094787
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-06-12 15:12:49 -0400

    Added user editing

--- forms.py
+++ forms.py
@@ -23,3 +23,13 @@ class LoginForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+
+class UserEditForm(FlaskForm):
+    """Form for editing a user"""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    image_url = StringField('Image URL')
+    header_image_url = StringField('Header Image Url')
+    bio = StringField('Bio')
+    password = PasswordField('Password', validators=[Length(min=6)])
